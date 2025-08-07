7c0f25a0268b96a82049a438a0173ff93fdebd61
mahesh-143 <maheshodedara13@yahoo.com>
2025-08-07 23:56:44 +0530

    feat(parser): parse commands arguments

--- shell.c
+++ shell.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main() {
+  char *prompt = "$ 🍋 >> ";
+  char input[256];
+  char *args[64];
+
   while (1) {
-    char input[256];
-    printf("hesh>> ");
+    printf("%s", prompt);
     if (fgets(input, sizeof(input), stdin) == NULL) {
       printf("EOF or error occurred\n");
       break;
     }
+
     input[strcspn(input, "\n")] = '\0';
+    char *token = strtok(input, " \t\n");
+
+    int i = 0;
+    while (token != NULL && i < 64) {
+      args[i++] = token;
+      token = strtok(NULL, " \t\n");
+    }
+    args[i] = NULL;
+
+    pid_t pid = fork();
+    if (pid == 0) {
+      execvp(args[0], args);
+      perror("exec failed");
+      exit(1);
+    } else {
+      wait(NULL);
+    }
     if (strcmp(input, "exit") == 0) {
       printf("exit\n");
       exit(0);
     }
   }
+
   return 0;
 }
